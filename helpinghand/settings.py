8a366608ab9cc18b21669cfdea9e090426b9c968
Avirup Mondal <mondalavirup2015@gmail.com>
2021-05-16 11:06:51 +0530

    2 commit

--- helpinghand/settings.py
+++ helpinghand/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'helpinghand.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,9 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
---
+++ helpinghandmain/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HelpinghandmainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'helpinghandmain'

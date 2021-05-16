ebbd943871de039f60b30b93a0be1d7169752dde
Avirup Mondal <mondalavirup2015@gmail.com>
2021-05-16 12:00:57 +0530

    2 commit

--- helpinghandmain/views.py
+++ helpinghandmain/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, HttpResponse
-# from bimbodesigner.models import Feedback, Contact
-# from django.contrib import messages
-# Create your views here.
+from datetime import datetime
+
 
 def index(request):
-    return render(request,'main/index.html')
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+    context={
+        'current_time': current_time
+    }
+    return render(request,'main/index.html', context=context)
 
 

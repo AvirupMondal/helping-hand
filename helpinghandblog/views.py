07c1dbf2532fc34cdd326e9e914e415dbc539a59
Avirup Mondal <mondalavirup2015@gmail.com>
2021-05-16 11:17:41 +0530

    2 commit

--- helpinghandblog/views.py
+++ helpinghandblog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, HttpResponse
+
+
+def blogindex(request):
+    return render(request,'blog/blog.html')
+
+
--- helpinghandmain/views.py
+++ helpinghandmain/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+# from bimbodesigner.models import Feedback, Contact
+# from django.contrib import messages
 # Create your views here.
+
+def index(request):
+    return render(request,'main/index.html')
+
+

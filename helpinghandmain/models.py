bdde697eb6bbae1964756d600e1e3ef5a4eb744f
Avirup Mondal <mondalavirup2015@gmail.com>
2021-05-16 16:18:37 +0530

    2 commit

--- helpinghandmain/models.py
+++ helpinghandmain/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Register(models.Model):
+
+    sno=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=500)
+    email=models.CharField(max_length=500)
+    password=models.CharField(max_length=500)
+    address=models.CharField(max_length=500)
+    state=models.CharField(max_length=500)
+    city=models.CharField(max_length=500)
+    pincode=models.CharField(max_length=500)
+    category=models.CharField(max_length=500)
+    contact=models.CharField(max_length=20)
+    
+
+    def __str__(self):
+        return 'New User' + self.name + (self.category)
--- helpinghandmain/views.py
+++ helpinghandmain/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from datetime import datetime
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from helpinghandmain.models import Register
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
 
 
 def index(request):
@@ -12,7 +21,29 @@ def index(request):
     return render(request,'main/index.html', context=context)
 
 def login(request):
-    return render(request,'main/login.html')
+    if request.method == 'POST':
+  
+        # AuthenticationForm_can_also_be_used__
+  
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email = email, password = password)
+        if user is not None:
+            form = login(request, user)
+            messages.success(request, f' wecome {email} !!')
+            return redirect('home_detail')
+        else:
+            messages.info(request, f'account does not exit. Please Register Yourself')
+    form = AuthenticationForm()
+    return render(request, 'main/login.html')
 
 def register(request):
     return render(request,'main/register.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+
+def home_detail(request):
+    return render(request,'main/home_detail.html')
